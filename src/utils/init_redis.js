666aca7db6d1f8780d09ba35c3d95a07b1f1cd3e
Doyin Awosanya <didoloan@gmail.com>
2021-01-16 03:20:18 +0100

    Added conditional redis url

--- src/utils/init_redis.js
+++ src/utils/init_redis.js
@@ -1,8 +1,8 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-  port: 6379,
-  host: '127.0.0.1',
+  port: process.env.ENV=='DEV'?6379:11263,
+  host: process.env.ENV=='DEV'?'127.0.0.1':'0.tcp.ngrok.io',
 })
 
 client.on('connect', () => {
